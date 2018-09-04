144f3b94d45816cd984e6ee0e3acd47fe043c07a
weijibin <1285253265@qq.com>
2018-09-04 17:49:44 +0800

    完善接口

--- FreeDrawingItem/freedrawingitem.cpp
+++ FreeDrawingItem/freedrawingitem.cpp
@@ -15,3 +15,63 @@ void FreeDrawingItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *o
 {
 
 }
+
+
+// creating
+void FreeDrawingItem::downWhenCreating(QPointF point)
+{
+
+}
+
+void FreeDrawingItem::moveWhenCreating(QPointF point)
+{
+
+}
+
+void FreeDrawingItem::upWhenCreating(QPointF point)
+{
+
+}
+
+// end creating
+void FreeDrawingItem::endCreate()
+{
+
+}
+
+// editing
+void FreeDrawingItem::downWhenEditing(QPointF point)
+{
+
+}
+
+void FreeDrawingItem::moveWhenEditing(QPointF point)
+{
+
+}
+
+void FreeDrawingItem::upWhenEditing(QPointF point)
+{
+
+}
+
+// translate item  or  call in command
+void FreeDrawingItem::translateItem(QPointF offset)
+{
+
+}
+
+// add command
+//    void addCommandToSlide(HHCommand * command);
+
+// save
+void FreeDrawingItem::getJSONObj( QJsonObject& jsonObj )
+{
+
+}
+
+// restore
+void FreeDrawingItem::fromJSONObj(QJsonObject& jsonObj )
+{
+
+}
--- FreeDrawingItem/freedrawingitem.h
+++ FreeDrawingItem/freedrawingitem.h
@@ -2,6 +2,7 @@
 #define FREEDRAWINGITEM_H
 
 #include <QGraphicsItem>
+#include <QPointF>
 #include "anchorpointinfo.h"
 #include "customitem.h"
 #include "anchorpointitem.h"
@@ -14,11 +15,39 @@ public:
     virtual QRectF boundingRect() const override;
     virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
+    // creating
+    void downWhenCreating(QPointF point);
+    void moveWhenCreating(QPointF point);
+    void upWhenCreating(QPointF point);
+
+    // end creating
+    void endCreate();
+
+    // editing
+    void downWhenEditing(QPointF point);
+    void moveWhenEditing(QPointF point);
+    void upWhenEditing(QPointF point);
+
+    // translate item  or  call in command
+    void translateItem(QPointF offset);
+
+    // add command
+//    void addCommandToSlide(HHCommand * command);
+
+    // save
+    void getJSONObj( QJsonObject& jsonObj );
+
+    // restore
+    void fromJSONObj(QJsonObject& jsonObj );
+
 private:
-    QVector<QPainterPath> m_subPaths;
+
+
+private:
+//    QVector<QPainterPath> m_subPaths;
     QVector<QGraphicsPathItem> m_subPathItems;
 
-    QVector<AnchorPointInfo> m_AnchorPoints;
+//    QVector<AnchorPointInfo> m_AnchorPoints;
     QVector<AnchorPointItem> m_AnchorPointItems;
 };
 
